6e9b33d1e3eb27524dccac4a5437da08cc463b33
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2024-06-07 17:25:06 -0300

    fix

--- main.py
+++ main.py
@@ -5,50 +5,46 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+service = Service("chromedriver/chromedriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("http://bianca.com")
+
+title = driver.title
+cookies = driver.get_cookies()
+text = driver.find_element(By.XPATH, '/html').text
+inner_html = driver.find_element(By.XPATH, '/html').get_attribute('innerHTML')
+is_selected = driver.find_element(By.XPATH, '/html').is_selected()
+css_value = driver.find_element(By.XPATH, '/html').value_of_css_property('propriedade_css')
+
+attributes_rows = []
+
+rows = driver.find_elements(By.XPATH, 'xpath_das_linhas')
+for row in rows:
+    cells = row.find_elements(By.TAG_NAME, 'td')
+    attributes_rows.append(cells)
+
+link = driver.find_element(By.XPATH, '/html').get_attribute('h1')
+
+items = driver.find_elements(By.XPATH, '/html')
+
+list_items = []
+for item in items:
+    list_items.append(item.text)
+
 
 @app.get("/bianca")
 async def root():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # Download the webdriver
-    service = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    service.implicitly_wait(5)
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    driver.get("http://bianca.com")
-
-    title = driver.title
-    cookies = driver.get_cookies()
-    text = driver.find_element(By.XPATH, '/html').text
-    inner_html = driver.find_element(By.XPATH, '/html').get_attribute('innerHTML')
-    is_selected = driver.find_element(By.XPATH, '/html').is_selected()
-    css_value = driver.find_element(By.XPATH, '/html').value_of_css_property('propriedade_css')
-
-    attributes_rows = []
-
-    rows = driver.find_elements(By.XPATH, 'xpath_das_linhas')
-    for row in rows:
-        cells = row.find_elements(By.TAG_NAME, 'td')
-        attributes_rows.append(cells)
-
-    link = driver.find_element(By.XPATH, '/html').get_attribute('h1')
-
-    items = driver.find_elements(By.XPATH, '/html')
-
-    list_items = []
-    for item in items:
-        list_items.append(item.text)
 
     driver.quit()
     return {'title': title,
