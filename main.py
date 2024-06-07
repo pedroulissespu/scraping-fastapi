4cb666a4e3f5caafcd34e2d69958db4bc14c7c37
Pedro Ulisses <pedro.ulisses@aluno.uece.br>
2024-06-07 16:32:30 -0300

    fix: selenium

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from typing import Union
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 from fastapi import FastAPI
 
 app = FastAPI()
